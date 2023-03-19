261b95479bac120b8c531fccaeb3c298fce6923f
Marah Salahat <114695055+MarahSalahat@users.noreply.github.com>
2023-03-19 22:32:03 +0200

    Add files via upload

---
+++ boy_or_girl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 22:07:27 2023
+
+@author: HP
+"""
+
+st= input()
+if (len(set(st))%2 != 0) :
+    print( "IGNORE HIM!" )
+else:
+    print("CHAT WITH HER!")
